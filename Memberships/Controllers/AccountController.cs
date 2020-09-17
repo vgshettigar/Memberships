4c0312b381fbaaadee7d954973ac7aef6c3714d6
vgshettigar <ganesha.shettigara@gmail.com>
2020-09-17 19:40:29 -0400

    updated a file

--- Memberships/Controllers/AccountController.cs
+++ Memberships/Controllers/AccountController.cs
@@ -91,7 +91,7 @@ namespace Memberships.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Invalid login attempt by user.");
                     return View(model);
             }
         }
